647c30de85b9ca9acec06840f678a62f6fc25d33
Adel Utyashev <adel99685@gmail.com>
2024-11-26 23:32:24 +0300

    Step 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,8 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
+    application
     id("java")
     id("checkstyle")
 }
@@ -6,12 +10,16 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application { mainClass.set("hexlet.code.App") }
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
